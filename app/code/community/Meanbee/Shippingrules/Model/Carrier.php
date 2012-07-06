22183c40f3b53c823d4bbd1803295310b14a5b99
Nick Jones <nick.jones@meanbee.com>
2012-07-06 23:03:56 +0100

    Only choose one rule with a title

--- app/code/community/Meanbee/Shippingrules/Model/Carrier.php
+++ app/code/community/Meanbee/Shippingrules/Model/Carrier.php
@@ -16,28 +16,51 @@ class Meanbee_Shippingrules_Model_Carrier extends Mage_Shipping_Model_Carrier_Ab
 
         $result = Mage::getModel('shipping/rate_result');
 
-        foreach (Mage::getModel('meanship/rule')->getCollection() as $rule) {
-
-            if (!$rule->validate($request)) {
-                continue;
-            }
-
+        foreach ($this->_getApplicableRules($request) as $method_name =>  $rule_data) {
             $method = Mage::getModel('shipping/rate_result_method');
 
             $method->setCarrier($this->_code);
             $method->setCarrierTitle('Shipping');
 
             // record method information
-            $method->setMethod($rule->getId());
-            $method->setMethodTitle($rule->getName());
+            $method->setMethod($rule_data->getId());
+            $method->setMethodTitle($method_name);
 
             // rate cost is optional property to record how much it costs to vendor to ship
-            $method->setCost($rule->getPrice());
-            $method->setPrice($rule->getPrice());
+            $method->setCost($rule_data->getCost());
+            $method->setPrice($rule_data->getPrice());
 
             $result->append($method);
         }
 
         return $result;
+    }
+
+    protected function _getApplicableRules(Mage_Shipping_Model_Rate_Request $request) {
+        $methods = array();
+
+        $rule_collection = Mage::getModel('meanship/rule')->getCollection()
+            ->addFieldToFilter('is_active', 1)
+            ->setOrder('sort_order');
+
+        foreach ($rule_collection as $rule) {
+            if (!$rule->validate($request)) {
+                continue;
+            }
+
+            if (array_key_exists($rule->getName(), $methods)) {
+                if ($methods[$rule->getName()]->getPrice() < $rule->getPrice()) {
+                    continue;
+                }
+            }
+
+            $methods[$rule->getName()] = new Varien_Object(array(
+                'price' => $rule->getPrice(),
+                'cost'  => $rule->getCost(),
+                'id'    => $rule->getId()
+            ));
+        }
+
+        return $methods;
     }
 }
