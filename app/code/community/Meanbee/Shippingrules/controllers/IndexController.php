bc63a5308a34e219f43f4b41342e809303c1c569
Nick Jones <nick.jones@meanbee.com>
2012-07-06 12:28:06 +0100

    Added basic controller and menu item

---
+++ app/code/community/Meanbee/Shippingrules/controllers/IndexController.php
@@ -0,0 +1,7 @@
+<?php
+class Meanbee_Shippingrules_IndexController extends Mage_Adminhtml_Controller_Action {
+    public function indexAction() {
+        $this->loadLayout();
+        $this->renderLayout();
+    }
+}
