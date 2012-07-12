7b4c358c30dcfb737d62765d0698f37678d40a19
Nick Jones <nick.jones@meanbee.com>
2012-07-12 15:10:58 +0100

    Downcase and strip spaces for postcode comparison

--- app/code/community/Meanbee/Shippingrules/Model/Rule/Condition/Destination/Zipcode.php
+++ app/code/community/Meanbee/Shippingrules/Model/Rule/Condition/Destination/Zipcode.php
@@ -14,4 +14,23 @@ class Meanbee_Shippingrules_Model_Rule_Condition_Destination_Zipcode extends Mea
         $element->setShowAsText(true);
         return $element;
     }
+
+    protected function _sanitise($str) {
+        return str_replace(' ', '', strtolower($str));
+    }
+
+    public function getValueParsed() {
+        if (!$this->hasValueParsed()) {
+            $value = $this->getData('value');
+            $this->setValueParsed($this->_sanitise($value));
+        }
+
+        return $this->getData('value_parsed');
+    }
+
+    public function validate(Varien_Object $object) {
+        return $this->validateAttribute(
+            $this->_sanitise($object->getData($this->getAttribute()))
+        );
+    }
 }
