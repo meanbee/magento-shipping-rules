7d8a3cc51a06197bc9297dd37cec12cf199d0584
Nick Jones <nick.jones@meanbee.com>
2013-04-02 14:01:56 +0100

    Reference the correct version of the Rules class for 1.6

--- app/code/community/Meanbee/Shippingrules/Model/Resource/Rule/Abstract.php
+++ app/code/community/Meanbee/Shippingrules/Model/Resource/Rule/Abstract.php
@@ -1,5 +1,5 @@
 <?php
-if (version_compare(Mage::getVersion(), '1.6', '>=')) {
+if (version_compare(Mage::getVersion(), '1.7', '>=')) {
     abstract class Meanbee_Shippingrules_Model_Resource_Rule_Abstract extends Mage_Rule_Model_Resource_Abstract {}
 } else {
     abstract class Meanbee_Shippingrules_Model_Resource_Rule_Abstract extends Mage_Rule_Model_Mysql4_Rule {}
