0d61d4cf3c202393f85cd6aa0f66b19da15bfde3
Paul Hachmang <paul@h-o.nl>
2013-05-13 14:20:40 +0200

    Possibility to calculate a product subselection based on a value added to the product.

--- app/code/community/Meanbee/Shippingrules/Model/Rule/Condition/Product/Subselect.php
+++ app/code/community/Meanbee/Shippingrules/Model/Rule/Condition/Product/Subselect.php
@@ -6,6 +6,41 @@ class Meanbee_Shippingrules_Model_Rule_Condition_Product_Subselect extends Mage_
             ->setValue(null);
     }
 
+
+    public function loadAttributeOptions()
+    {
+        $hlp = Mage::helper('salesrule');
+        $attributes = array(
+            'qty'  => $hlp->__('total quantity'),
+            'base_row_total'  => $hlp->__('total amount'),
+        );
+
+        $productAttributes = Mage::getResourceSingleton('catalog/product')
+           ->loadAllAttributes()
+           ->getAttributesByCode();
+        $allowedInputTypes = array('text', 'boolean', 'price');
+        $allowedTextValidations = array('validate-number', 'validate-digits');
+
+        foreach ($productAttributes as $attribute) {
+           /* @var $attribute Mage_Catalog_Model_Resource_Eav_Attribute */
+           if (!$attribute->getIsVisible()
+               || !in_array($attribute->getFrontendInput(), $allowedInputTypes)
+               || !$attribute->getDataUsingMethod('is_used_for_promo_rules')
+               || ($attribute->getFrontendInput() == 'text' && ! in_array($attribute->getFrontendClass(), $allowedTextValidations))
+           ) {
+               continue;
+           }
+
+           $attributes[$attribute->getAttributeCode()] = $hlp->__('Sum of attribute: %s',$attribute->getFrontendLabel());
+        }
+
+        asort($attributes);
+        $this->setAttributeOption($attributes);
+
+        return $this;
+    }
+
+
     /**
      * validate
      *
@@ -20,8 +55,14 @@ class Meanbee_Shippingrules_Model_Rule_Condition_Product_Subselect extends Mage_
         $attr = $this->getAttribute();
         $total = 0;
         foreach ($object->getData('all_items') as $item) {
+            /** @var $item Mage_Sales_Model_Quote_Item */
             if ($this->_validateItem($item)) {
-                $total += $item->getData($attr);
+                if ($item->getData($attr)) {
+                    $total += (float) $item->getData($attr);
+                } elseif ($item->getProduct() instanceof Mage_Catalog_Model_Product) {
+                    $product = Mage::getModel('catalog/product')->load($item->getProduct()->getId());
+                    $total += (float) $product->getData($attr) * $item->getQty();
+                }
             }
         }
 
@@ -43,6 +84,7 @@ class Meanbee_Shippingrules_Model_Rule_Condition_Product_Subselect extends Mage_
         $all = $this->getAggregator() === 'all';
 
         foreach ($this->getConditions() as $cond) {
+            /** @var Mage_SalesRule_Model_Rule_Condition_Product $cond */
             $validated = $cond->validate($object);
 
             if ($all && $validated !== true) {
