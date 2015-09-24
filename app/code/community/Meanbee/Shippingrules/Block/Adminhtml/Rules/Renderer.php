28d265bd262f29a2c47b954cc0aa718535657202
Ashley Wright <ashley.wright@meanbee.com>
2015-09-24 16:47:25 +0100

    SHIP-11: Adds a custom column renderer for the conditions in the grid. Adds a custom column renderer for the shipping rule conditions as displayed in the shipping rules grid view in the administration console, which renders similarly to the interface through which the user inputs the conditions. However with this renderer it will give a vertical elipsis after the 5th condition in a condition group to prevent them becoming unwieldly.

---
+++ app/code/community/Meanbee/Shippingrules/Block/Adminhtml/Rules/Renderer.php
@@ -0,0 +1,25 @@
+<?php
+
+class Meanbee_Shippingrules_Block_Adminhtml_Rules_Renderer
+	extends Mage_Adminhtml_Block_Widget_Grid_Column_Renderer_Abstract
+{
+	public function render(Varien_Object $row)
+	{
+		$conditions = $row->getConditions();
+		Mage::log($conditions, Zend_Log::DEBUG, 'session.log', true);
+		return $this->asHtmlRecursive($conditions);
+	}
+	protected function asHtmlRecursive($condition)
+    {
+        $html = $condition->asString().'<ul id="'.$condition->getPrefix().'__'.$condition->getId().'__children" class="rule-param-children" style="padding-left: 1em">';
+        foreach ($condition->getConditions() as $i => $child) {
+			if ($i >= 5) {
+				$html .='<li>⋮</li>';
+				break;
+			}
+            $html .= '<li>'.$this->asHtmlRecursive($child).'</li>';
+        }
+        $html .= '</ul>';
+        return $html;
+    }
+}
