4c562cf639800c2ea2bafaf44131ce7d98c818c0
Ashley Wright <ashley.wright@meanbee.com>
2015-09-28 16:18:18 +0100

    SHIP-19: Removes redundant SQL Update Statement.

--- app/code/community/Meanbee/Shippingrules/sql/meanship_setup/mysql4-upgrade-2.1.1-2.1.2.php
+++ app/code/community/Meanbee/Shippingrules/sql/meanship_setup/mysql4-upgrade-2.1.1-2.1.2.php
@@ -10,8 +10,4 @@ $installer->run("
   ALTER TABLE `{$table_name}` ADD `notes` TEXT NOT NULL DEFAULT '';
 ");
 
-$installer->run("
-  UPDATE `{$table_name}` SET `notes`='' WHERE `notes` IS NULL;
-");
-
 $this->endSetup();
