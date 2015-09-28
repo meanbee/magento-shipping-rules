821a8fb5e3634b496609c96a4ee905bd1de65ce6
Ashley Wright <ashley.wright@meanbee.com>
2015-09-28 16:03:23 +0100

    SHIP-24: Removes redundant SQL Update statement.

--- app/code/community/Meanbee/Shippingrules/sql/meanship_setup/mysql4-upgrade-2.1.2-2.1.3.php
+++ app/code/community/Meanbee/Shippingrules/sql/meanship_setup/mysql4-upgrade-2.1.2-2.1.3.php
@@ -10,8 +10,4 @@ $installer->run("
   ALTER TABLE `{$table_name}` ADD `stop_all_rules_processing` TINYINT(1) NOT NULL DEFAULT 0;
 ");
 
-$installer->run("
-  UPDATE `{$table_name}` SET `stop_all_rules_processing`=0 WHERE `stop_all_rules_processing` IS NULL;
-");
-
 $this->endSetup();
