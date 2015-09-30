ec86abf5cb369af9bc9af06e6c914a88ebac51fa
Ashley Wright <ashley.wright@meanbee.com>
2015-09-30 15:16:47 +0100

    SHIP-11: Make indentation consistent.

--- app/code/community/Meanbee/Shippingrules/Block/Adminhtml/Rules/Renderer.php
+++ app/code/community/Meanbee/Shippingrules/Block/Adminhtml/Rules/Renderer.php
@@ -1,14 +1,14 @@
 <?php
 
 class Meanbee_Shippingrules_Block_Adminhtml_Rules_Renderer
-	extends Mage_Adminhtml_Block_Widget_Grid_Column_Renderer_Abstract
+    extends Mage_Adminhtml_Block_Widget_Grid_Column_Renderer_Abstract
 {
-	public function render(Varien_Object $row)
-	{
-		$conditions = $row->getConditions();
-		return $this->asHtmlRecursive($conditions);
-	}
-	protected function asHtmlRecursive($condition)
+    public function render(Varien_Object $row)
+    {
+        $conditions = $row->getConditions();
+        return $this->asHtmlRecursive($conditions);
+    }
+    protected function asHtmlRecursive($condition)
     {
         $html = $condition->asString().'<ul id="'.$condition->getPrefix().'__'.$condition->getId().'__children" class="rule-param-children" style="padding-left: 1em">';
         foreach ($condition->getConditions() as $i => $child) {
