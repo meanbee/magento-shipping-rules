108f15c32ae579968e59274bf4d8569e50c334a5
Ashley Wright <ashley.wright@meanbee.com>
2015-12-21 10:26:12 +0000

    SHIP-21: Document postal code parts format types.

--- app/code/community/Meanbee/Shippingrules/Helper/Postcode.php
+++ app/code/community/Meanbee/Shippingrules/Helper/Postcode.php
@@ -4,7 +4,34 @@ class Meanbee_Shippingrules_Helper_Postcode extends Mage_Core_Helper_Abstract {
     const NUMERIC_BASE10 = 'b10'; // Postcode part should be considered as a decimal integer.
     const NUMERIC_BASE26 = 'b26'; // Postcode part should be considered as a hexavigesimal integer.
     const NUMERIC_BASE36 = 'b36'; // Postcode part should be considered as a hexatrigesimal integer.
-    const CONSTANT = '';          // Postcode part should not be cosidered for solitary comparison since it is constant for the format.
+    const CONSTANT = '';          // Postcode part should not be considered for solitary comparison since it is constant for the format.
+    /**
+     * For example, the GB postcode is one of the formats:
+     * AA9A 9AA, A9A 9AA, A9 9AA, A99 9AA, AA9 9AA, AA99 9AA
+     * where 9 represents a character in the range 0..9 and A represent a character
+     * in the range A..Z.
+     *
+     * Take the example cases BA1 1EF and N1P 7EG.
+     * The GB postcode is subdivided into the sections:
+     * Outward Code (BA1, N1P) and Inward Code (1EF, 7EG).
+     * Outward Code is subdivided into the sections:
+     * Postcode Area (BA, N) and Postcode District (1, 1P).
+     * Inward Code is subdivided into the sections:
+     * Postcode Sector (1, 7) and Postcode Unit (EF, EG).
+     *
+     * Therefore, the GB format postal code is considered in the 4 parts:
+     * Postcode Area, Postcode District, Postcode Sector and Postcode Unit.
+     *
+     * For Postcode District, Postcode Sector and Postcode Unit is makes sense to
+     * compare them numerically, as such they are considered as NUMERIC_BASE36,
+     * NUMERIC_BASE10 and NUMERIC_BASE26 respectively. It does not make sense to
+     * compare Postcode Area numerically and so it is considered as ALPHABETIC.
+     *
+     * NUMERIC_* parts have operators: greater than, less than, etc.
+     * ALPHABETIC parts have operators: begins with, ends with, matches regex, etc.
+     * CONSTANT parts do not have any operators since any unnegatedcomparison would
+     * return true.
+     */
 
     /**
      * Loads postal code format descriptors from JSON file.
