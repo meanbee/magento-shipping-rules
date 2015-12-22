b8fef6d209034f666a8136d00bb11ab9200629e5
Ashley Wright <ashley.wright@meanbee.com>
2015-12-22 11:43:52 +0000

    SHIP-11: Prettify postal code conditions.

--- app/code/community/Meanbee/Shippingrules/Block/Adminhtml/Rules/Renderer.php
+++ app/code/community/Meanbee/Shippingrules/Block/Adminhtml/Rules/Renderer.php
@@ -72,6 +72,39 @@ class Meanbee_Shippingrules_Block_Adminhtml_Rules_Renderer
                 }
                 return $condition->getAttributeName() . ' ' . $this->renderOperator($condition) . ' ' . join(', ', $countries);
                 break;
+            case NULL:
+                switch ($condition->getType()) {
+                    case 'meanship/rule_condition_postalCode':
+                        $aggregator = strtoupper($condition->getAggregator());
+                        switch (Mage::helper('meanship/config')->getCondenseCountriesOnGrid()) {
+                            case Meanbee_Shippingrules_Model_Config_Source_CountryCondensation::COUNTRY_CODE:
+                                $value = $condition->getValue();
+                                break;
+                            case Meanbee_Shippingrules_Model_Config_Source_CountryCondensation::REGIONAL_IDENTIFIERS:
+                                $countryHelper = Mage::helper('meanship/country');
+                                $useEmojiOne = Mage::helper('meanship/config')->getUseEmojiOne();
+                                $emojiHelper = Mage::helper('meanship/emoji');
+                                if ($useEmojiOne) {
+                                    $match = false;
+                                    $imageCode = $emojiHelper->unicodeToImage($countryHelper->toRegionalIndicatorSymbols($condition->getValue()), $match);
+                                    if ($match) {
+                                        $value = '<abbr title="' . $condition->getValueName() . '">' . $imageCode . '</abbr>';
+                                    } else {
+                                        $value = $condition->getValue();
+                                    }
+                                } else {
+                                    $value = '<abbr title="' . $condition->getValueName() . '">' .
+                                                $countryHelper->toRegionalIndicatorSymbols($condition->getValue()) . '</abbr>';
+                                }
+                                break;
+                            case Meanbee_Shippingrules_Model_Config_Source_CountryCondensation::VERBOSE:
+                            default:
+                                $value = $condition->getValueName();
+                        }
+                        return "Postal Code of ${value} matches ${aggregator} of these conditions:";
+                    default:
+                        return $condition->asString();
+                }
             default:
                 return $condition->getAttributeName() . ' ' . $this->renderOperator($condition) . ' ' . $condition->getValueName();
         }
