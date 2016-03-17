193ae760ae507fb2fdee55522418f98ce448d035
Ashley Wright <ashley.wright@meanbee.com>
2016-03-17 15:02:28 +0000

    SHIP-40: Add handling of styles to tooling.

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,16 @@
-var gulp       = require('gulp'),
-    babel      = require('gulp-babel'),
-    concat     = require('gulp-concat'),
-    nativejsx  = require('gulp-nativejsx'),
-    sourcemaps = require('gulp-sourcemaps'),
-    uglify     = require('gulp-uglify');
+var gulp         = require('gulp'),
+    autoprefixer = require('gulp-autoprefixer'),
+    babel        = require('gulp-babel'),
+    concat       = require('gulp-concat'),
+    nativejsx    = require('gulp-nativejsx'),
+    sourcemaps   = require('gulp-sourcemaps'),
+    stylus       = require('gulp-stylus'),
+    uglify       = require('gulp-uglify');
 
 gulp.task('scripts', function() {
-  return gulp.src([
-              'src/skin/adminhtml/default/default/js/meanbee/shippingrules/*.js',
-              '!src/skin/adminhtml/default/default/js/meanbee/shippingrules/script.js',
+    return gulp.src([
+              'src/skin/adminhtml/base/default/js/meanbee/shippingrules/*.js',
+              '!src/skin/adminhtml/base/default/js/meanbee/shippingrules/script.js',
               'node_modules/gulp-nativejsx/node_modules/nativejsx/dist/jsxdom-prototypes.js'
              ])
              .pipe(sourcemaps.init())
@@ -17,11 +19,26 @@ gulp.task('scripts', function() {
              .pipe(concat('script.js'))
              .pipe(uglify())
              .pipe(sourcemaps.write('.'))
-             .pipe(gulp.dest('src/js'));
+             .pipe(gulp.dest('src/skin/adminhtml/base/default/js/meanbee/shippingrules'));
 });
 
-gulp.task('watch', function() {
-  return gulp.watch(['src/skin/adminhtml/default/default/js/meanbee/shippingrules/*.js', '!src/skin/adminhtml/default/default/js/meanbee/shippingrules/script.js'], ['scripts']);
+gulp.task('stylus', function () {
+    return gulp.src([
+                'src/skin/adminhtml/base/default/css/meanbee/shippingrules/application.styl'
+               ])
+               .pipe(sourcemaps.init())
+               .pipe(stylus())
+               .pipe(autoprefixer({browsers: ['last 2 versions']}))
+               .pipe(sourcemaps.write('.'))
+               .pipe(gulp.dest('src/skin/adminhtml/base/default/css/meanbee/shippingrules'))
 });
 
-gulp.task('default', ['watch', 'scripts']);
+gulp.task('watch', ['stylus', 'scripts'], function() {
+    return gulp.watch([
+                        'src/skin/adminhtml/base/default/css/meanbee/shippingrules/*.styl',
+                        'src/skin/adminhtml/base/default/js/meanbee/shippingrules/*.js',
+                        '!src/skin/adminhtml/base/default/js/meanbee/shippingrules/script.js'
+                    ], ['stylus', 'scripts']);
+});
+
+gulp.task('default', ['watch']);
