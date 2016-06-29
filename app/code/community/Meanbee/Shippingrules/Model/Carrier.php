3373f6a63bc80ec4a1d4c50ec83af9a6390fd093
Ashley Wright <ashley.wright@meanbee.com>
2016-06-29 13:43:34 +0100

    SHIP-49: Extract logic for getting ccart price rules into own method.

--- app/code/community/Meanbee/Shippingrules/Model/Carrier.php
+++ app/code/community/Meanbee/Shippingrules/Model/Carrier.php
@@ -288,18 +288,31 @@ class Meanbee_Shippingrules_Model_Carrier extends Mage_Shipping_Model_Carrier_Ab
             $quote = $requestItems[0]->getQuote();
             $request->setData('promo_free_shipping', $quote->getShippingAddress()->getFreeShipping());
             $request->setData('promo_coupon_code', $quote->getCouponCode());
-            $request->setData(
-                'promo_applied_rule_ids',
-                implode(',', array_unique(explode(',', array_reduce(
-                    $request->getAllItems(),
-                    function ($accumulator, $product) {
-                        return implode(',', array_filter(array($accumulator, $product->getAppliedRuleIds())));
-                    },
-                    $quote->getAppliedRuleIds() ?: ''
-                ))))
-            );
+            $request->setData('promo_applied_rule_ids', implode(',', $this->getAppliedCartPriceRules($requestItems)) ?: null);
         }
         return $request;
+    }
+
+    /**
+     * Gets an array of ids of all cart price rules that have been applied to
+     * the cart or any product in the cart, without duplicates, regardless of
+     * whether or not the billing and delivery addresses differ.
+     * @param  Array $requestItems Array of items from the
+     *                             Mage_Shipping_Model_Rate_Request object.
+     * @return string[]
+     */
+    protected function getAppliedCartPriceRules($requestItems) {
+        if (count($requestItems) > 0) {
+            $quote = $requestItems[0]->getQuote();
+            return array_unique(explode(',', array_reduce(
+                $requestItems,
+                function ($accumulator, $product) {
+                    return implode(',', array_filter(array($accumulator, $product->getAppliedRuleIds())));
+                },
+                $quote->getAppliedRuleIds() ?: ''
+            )));
+        }
+        return array();
     }
 
     /**
