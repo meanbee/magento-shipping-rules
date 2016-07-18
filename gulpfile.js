0b9f8b9c291dd6ae4e928b8cce47090f8371b0a3
Ashley Wright <ashley.wright@meanbee.com>
2016-07-18 10:50:54 +0100

    SHIP-40: Add test task to gulpfile to run unit tests.

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,34 @@
-/* global require:false */
+/* global require:false, process:false */
 'use strict';
 var gulp         = require('gulp'),
     autoprefixer = require('gulp-autoprefixer'),
     babel        = require('gulp-babel'),
     concat       = require('gulp-concat'),
+    gutil        = require('gulp-util'),
     nativejsx    = require('gulp-nativejsx'),
     sourcemaps   = require('gulp-sourcemaps'),
     stylus       = require('gulp-stylus'),
-    uglify       = require('gulp-uglify');
+    // uglify       = require('gulp-uglify'),
+    spawn        = require('child_process').spawn;
+
+gulp.task('test', ['test:php']);
+
+gulp.task('test:php', function (done) {
+    let child = spawn('phpunit', [], { cwd: process.cwd() }),
+        stdout = '';
+    child.stdout.setEncoding('utf8');
+    child.stdout.on('data', function (data) {
+        stdout += data;
+    });
+    child.stderr.setEncoding('utf8');
+    child.stderr.on('data', function (data) {
+        stdout += gutil.colors.red(data);
+    });
+    child.on('close', function (code) {
+        gutil.log(stdout);
+        if (code !== 0) done('error');
+    })
+});
 
 gulp.task('scripts', function() {
     return gulp.src([
