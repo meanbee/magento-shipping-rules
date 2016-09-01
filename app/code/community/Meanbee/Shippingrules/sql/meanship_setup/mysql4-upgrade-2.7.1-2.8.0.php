0b8909101f3c3188ea3dea46d9517dc610ac33ff
Ashley Wright <ashley.wright@meanbee.com>
2016-09-01 16:30:33 +0100

    Add mechanism to prepare rules for migration to version 3.0.0 of Shipping Rules.

--- app/code/community/Meanbee/Shippingrules/sql/meanship_setup/mysql4-upgrade-2.7.1-2.8.0.php
+++ app/code/community/Meanbee/Shippingrules/sql/meanship_setup/mysql4-upgrade-2.7.1-2.8.0.php
@@ -11,6 +11,7 @@ $installer->run("
   ALTER TABLE `{$table_name}` ADD `price_migration` TEXT NOT NULL DEFAULT '';
   ALTER TABLE `{$table_name}` ADD `cost_migration` TEXT NOT NULL DEFAULT '';
   ALTER TABLE `{$table_name}` ADD `migrated_to` TEXT NOT NULL DEFAULT '';
+  ALTER TABLE `{$table_name}` ADD `migration_ignored` TINYINT(1) UNSIGNED NOT NULL DEFAULT 0;
 ");
 
 $rules = Mage::getModel('meanship/rule')->getCollection();
